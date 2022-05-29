a714ec9bb226636e4829ffdefd14fee4ad560088
Dganit Eger <dev@mbpvondaniteger.localdomain>
2022-05-29 12:38:11 +0200

    added a component

---
+++ Components/MyFirstComponent/MyFirstComponents.tsx
@@ -0,0 +1,9 @@
+import { Button } from "@mui/material";
+
+export const MyFirstComponent = () => {
+  return (
+    <>
+      <Button color="secondary">hello from component</Button>
+    </>
+  );
+};
